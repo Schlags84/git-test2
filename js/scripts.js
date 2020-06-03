8f98c3f6bf730e6fd821772e69f6308ad4be72af
Chris Schlagheck <christopherschlagheck@gmail.com>
2020-06-03 14:12:31 -0700

    Index.html Scripts

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$( document).ready(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselPause").click(function(){
+        $(".carousel").carousel("pause");
+    });
+    $("#carouselPlay").click(function(){
+        $(".carousel").carousel("cycle");
+    });
+});
