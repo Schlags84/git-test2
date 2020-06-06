24630979ab2e8b09d4f6e03675292694146e5cfd
Chris Schlagheck <christopherschlagheck@gmail.com>
2020-06-06 08:46:42 -0700

    Modal Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ $( document).ready(function() {
         $(".carousel").carousel("cycle");
     });
 });
+
+var modal = document.getElementById("reserveButton");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
