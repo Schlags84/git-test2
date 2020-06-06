c84129945bd7cbb1323c9ca2752ebd1781fe24ec
Chris Schlagheck <christopherschlagheck@gmail.com>
2020-06-06 08:57:59 -0700

    working on jquery modal

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ $( document).ready(function() {
     });
 });
 
-var modal = document.getElementById("reserveButton");
-var btn = document.getElementById("myBtn");
+var modal = document.getElementById("reserveModal");
+var btn = document.getElementById("reserveButton");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
